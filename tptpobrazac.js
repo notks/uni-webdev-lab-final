998e4673dbe8fff6e8a17a1d790b858fc844ee66
notks <kemal.selimbasic.2016@gmail.com>
2021-06-09 00:48:33 +0200

    product page done

--- tptpobrazac.js
+++ tptpobrazac.js
@@ -3,11 +3,7 @@ var email = document.getElementById("email");
 var telefon = document.getElementById("telefon");
 var poruka = document.getElementById("poruka");
 var btn = document.getElementById("formbtn");
-console.log(ime);
-console.log(email);
-console.log(telefon);
-console.log(poruka);
-console.log(btn);
+
 btn.addEventListener("click", () => {
   if (!ime.value || !email.value || !telefon.value || !poruka.value) {
     alert("Niste popunili sva polja");
