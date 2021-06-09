f280dcf0ef6dfce8cac203f09ba63ba5c09f1043
notks <kemal.selimbasic.2016@gmail.com>
2021-06-09 16:37:28 +0200

    ghpages deploy

--- tptpobrazac.js
+++ tptpobrazac.js
@@ -3,11 +3,23 @@ var email = document.getElementById("email");
 var telefon = document.getElementById("telefon");
 var poruka = document.getElementById("poruka");
 var btn = document.getElementById("formbtn");
+var cartBtn = document.getElementById("addToCart");
+var a = (e) => {
+  console.log(e.target);
+};
+console.log(cartBtn);
+try {
+  cartBtn.addEventListener("click", () => {
+    console.log("click");
+  });
+} catch (error) {}
 
-btn.addEventListener("click", () => {
-  if (!ime.value || !email.value || !telefon.value || !poruka.value) {
-    alert("Niste popunili sva polja");
-  } else {
-    alert("Uspjesno ste poslali poruku");
-  }
-});
+try {
+  btn.addEventListener("click", () => {
+    if (!ime.value || !email.value || !telefon.value || !poruka.value) {
+      alert("Niste popunili sva polja");
+    } else {
+      alert("Uspjesno ste poslali poruku");
+    }
+  });
+} catch (error) {}
