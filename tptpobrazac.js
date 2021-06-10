1dc7ad2bd553d3da4c5cb8358d3bd96fb4850d4d
notks <kemal.selimbasic.2016@gmail.com>
2021-06-10 04:14:17 +0200

    finished cart page

--- tptpobrazac.js
+++ tptpobrazac.js
@@ -5,9 +5,18 @@ var poruka = document.getElementById("poruka");
 var btn = document.getElementById("formbtn");
 var cartBtn = document.getElementById("addToCart");
 var a = (e) => {
-  console.log(e.target);
+  console.log(localStorage.getItem("cart"));
+  let storageCart = JSON.parse(localStorage.getItem("cart"));
+  let cart = storageCart ? storageCart : [];
+  let cartItem = {
+    item: e.path[2].accessKey,
+    img: e.path[2].childNodes[1].href,
+  };
+
+  cart.push(cartItem);
+  localStorage.setItem("cart", JSON.stringify(cart));
 };
-console.log(cartBtn);
+
 try {
   cartBtn.addEventListener("click", () => {
     console.log("click");
@@ -23,3 +32,56 @@ try {
     }
   });
 } catch (error) {}
+var loadCart = () => {
+  let totalPrice = 0;
+  let main = document.getElementById("main_cart");
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  cart.forEach((item) => {
+    let proizvod = document.createElement("div");
+    proizvod.classList.add("proizvod_cart");
+    let img_link = document.createElement("a");
+    img_link.href = item.img;
+    let img = document.createElement("img");
+    img.src = item.img;
+    img_link.appendChild(img);
+    proizvod.appendChild(img_link);
+
+    let title = document.createElement("span");
+    title.textContent = item.item;
+    proizvod.appendChild(title);
+    let price = document.createElement("div");
+    price.classList.add("price_cart");
+    let amount = document.createElement("span");
+    amount.textContent = "20";
+    price.appendChild(amount);
+    let currency = document.createElement("p");
+    currency.textContent = "KM";
+    price.appendChild(currency);
+    proizvod.appendChild(price);
+    main.appendChild(proizvod);
+    console.log(main);
+    totalPrice = totalPrice + 20;
+  });
+  let price = document.createElement("div");
+  price.classList.add("price_cart");
+  let amount = document.createElement("span");
+  amount.textContent = totalPrice;
+  price.appendChild(amount);
+  let currency = document.createElement("p");
+  currency.textContent = "KM";
+  price.appendChild(currency);
+  main.appendChild(price);
+};
+var buy = () => {
+  localStorage.removeItem("cart");
+  alert("Uspjesno ste izvrsili kupovinu");
+  window.location.reload(true);
+};
+let clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+  clear();
+});
+var clear = () => {
+  localStorage.removeItem("cart");
+  window.location.reload(true);
+};
